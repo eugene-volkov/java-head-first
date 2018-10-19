a010d22b01f6c5ee7a8b70f7b2e9183b9c229923
Eugene Volkov <bizzar@list.ru>
2018-10-20 00:18:07 +0300

    add ExerciselbA, ExerciselbB, ExerciselbC

---
+++ ExerciselbA.java
@@ -0,0 +1,11 @@
+class ExerciselbA {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 10) {
+            if (x > 3) {
+                System.out.println("большой икс");
+            }
+            x = x + 1;
+        }
+    }
+}
