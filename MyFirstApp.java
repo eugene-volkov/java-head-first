1c71b8321fe64d7f6a210396ae7f1619a86b6d03
Eugene Volkov <bizzar@list.ru>
2018-10-19 21:36:06 +0300

    init

---
+++ MyFirstApp.java
@@ -0,0 +1,9 @@
+//cmd support utf-8
+//chcp 65001
+
+public class MyFirstApp {
+	public static void main (String[] args) {
+		System.out.println("Я управляю");
+		System.out.println("миром");
+	}
+}
